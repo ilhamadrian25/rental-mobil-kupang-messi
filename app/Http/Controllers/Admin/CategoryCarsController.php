86ebc5c34a0b6ab8ee73bf3de510e5ae89f0b636
ilhamdotcom <muhamadilhamadrian0@gmail.com>
2023-08-10 16:56:02 +0700

    updated

--- app/Http/Controllers/Admin/CategoryCarsController.php
+++ app/Http/Controllers/Admin/CategoryCarsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\CategoryCarsModel;
+use Validator;
 
 class CategoryCarsController extends Controller
 {
@@ -15,5 +16,35 @@ class CategoryCarsController extends Controller
         ];
 
         return view('admin.category_cars.index', $data);
+    }
+
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(),[
+            'name' =>'required|max:30',
+            'slug' =>'nullable|max:30|unique:category_cars',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'message'   =>  $validate->errors()
+            ], 400);
+        }
+
+        $category = new CategoryCarsModel();
+
+        $category->name = $request->name;
+
+        if (!$request->slug) {
+            $category->slug = strtolower(trim(preg_replace('/[^a-zA-Z0-9-]+/', '-', $request->name), '-'));
+        }else {
+            $category->slug = strtolower(trim(preg_replace('/[^a-zA-Z0-9-]+/', '-', $request->slug), '-'));
+        }
+        
+        $category->save();
+
+        return response()->json([
+          'message'   =>  'Berhasil menambah data'
+        ], 200);
     }
 }
