1e3429f9de1c07d13228336549e65fb2eb112434
ilhamdotcom <muhamadilhamadrian0@gmail.com>
2023-08-22 09:53:19 +0700

    updateeeee

--- app/Http/Controllers/Frontend/ServicesController.php
+++ app/Http/Controllers/Frontend/ServicesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\CategoryModel;
 use App\Models\AddressModel;
+use App\Models\MetaModel;
 use App\Models\SocialMediaModel;
 use App\Models\SettingModel;
 
@@ -17,6 +18,7 @@ class ServicesController extends Controller
             'social' => SocialMediaModel::all(),
             'address' => AddressModel::first(),
             'settings' => SettingModel::first(),
+            'meta' => MetaModel::first(),
         ];
 
         return view('frontend.services.index', $data);
